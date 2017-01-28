ed96973cb67a8077b59c9c88fbc2eabc52c4400d
Peng Cong <buptpengcong@outlook.com>
2017-01-28 13:02:23 -0800

    update poly and final version v2

--- Project1/p1p2poly.py
+++ Project1/p1p2poly.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import *
-from sklearn import linear_model 
+from sklearn import linear_model
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
 from numpy import genfromtxt
-from scipy.interpolate import *
+from sklearn import cross_validation, linear_model
 import math
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
@@ -20,46 +20,54 @@ col_cut = 13
 my_feature = my_data[:, :col_cut]
 my_target  = my_data[:, col_cut]
 
-# color options before plot
-colors = ['blue' ,'green','red', 'gold']
+# get the best degree of freedom
+degrees = range(1,10)
+rmse = []
+print "Testing for the best degree..."
+
+for deg in range(len(degrees)):
+    polyFeatures = PolynomialFeatures(degree= degrees[deg],interaction_only=True, include_bias= False)
+    lm = linear_model.LinearRegression()
+
+    pipeline = Pipeline([("polynomial_features", polyFeatures),
+                             ("linear_regression", lm)])
+
+    pipeline.fit(my_feature,my_target)
+    scores = cross_validation.cross_val_score(pipeline, my_feature, my_target, cv=10)
+
+    rmse.append(np.mean(abs(scores)**0.5))
+
+# plot graph for RMSE vs. degree of polynomial
+plt.figure(1)
+plt.plot(degrees,rmse)
+plt.ylabel("RMSE")
+plt.xlabel("Polynomial Degree")
+plt.title("RMSE vs Polynomial Degree")
+
+
+degrees = [degrees[rmse.index(min(rmse))]]
+rmse = []
+
+# apply transformation on dataset to the best degree of freedom and then find error
+for deg in range(len(degrees)):
+    data_X_train, data_X_test, data_y_train, data_y_test = train_test_split(my_feature, my_target,  random_state=random.randrange(0, 100), test_size=0.1)
+    polyFeatures = PolynomialFeatures(degree= degrees[deg],interaction_only=True, include_bias= False)
+    X_train_trans = polyFeatures.fit_transform(data_X_train)
+    X_test_trans = polyFeatures.fit_transform(data_X_test)
+
+    lm = linear_model.LinearRegression()
+    lm.fit(X_train_trans,data_y_train)
+    predicted = lm.predict(X_test_trans)
+
+    rmse.append(np.mean(abs(predicted-data_y_test)**0.5))
 
 plt.figure(1)
-# Create polynomial regression
-for count, degree in enumerate([4,5,6,7]):
-	# initial the polynomial model
-    model = Pipeline([('poly', PolynomialFeatures(degree)),
-					('linear', LinearRegression())])
-    i = 0
-    max_score = float('-inf')
-    while i < 10 :
-        data_X_train, data_X_test, data_y_train, data_y_test = train_test_split(my_feature, my_target, test_size=0.1, random_state=random.randrange(0, 100))
-        train_set_vol = len(data_X_test)
-        poly = model.fit(data_X_train, data_y_train)
-        score = poly.score(data_X_test, data_y_test)
-        print score
-        if score > max_score:
-            max_score = score
-            optimal_coef = model.named_steps['linear'].coef_
-            best_predict = poly.predict(data_X_test)
-            optimal_RMSE = math.sqrt(mean_squared_error(data_y_test, best_predict))
-        i = i + 1
-    # Attributes of the model
-	# The coefficients
-    print('Coefficients:', optimal_coef)
-    # The root mean squared error
-    print("Root Mean Squared Error: %.6f" % optimal_RMSE)
-    # Explained variance score: 1 is perfect prediction
-    print("Variance score: %.6f" % score)
-    plt.scatter(data_y_test, best_predict, alpha=0.3, color=colors[count], label='degree %d' % degree, linewidth=1)
-    # plt.scatter(data_y_test, best_predict-data_y_test, alpha=0.6, color=colors[count], linewidth=1)
+plt.scatter(degrees,rmse)
+plt.ylabel("RMSE")
+plt.xlabel("Polynomial Degree")
+plt.title("RMSE vs Polynomial Degree")
 
+print "Best Degree of Polynomial : " + str(degrees[0])
+print "Root Mean Squared Error for Best Parameters : " + str(rmse[0])
 
-plt.plot([data_y_test.min(), data_y_test.max()], [0, 0], 'k--', alpha=0.3, lw=1.5)    
-plt.title('Residuals versus fitted values plot')
-plt.xlabel('Fitted Value')
-plt.ylabel('Residuals')
-plt.title('Fitted values and actual values scattered plot')
-plt.xlabel('Actual Value')
-plt.ylabel('Fitted Value')
-plt.legend(loc='upper left')
 plt.show()
