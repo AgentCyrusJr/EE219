79440992b06601cbacbc78a2f973ad5469788797
Peng Cong <buptpengcong@outlook.com>
2017-02-09 15:02:29 -0800

    update on Project, until problem(e)

--- Project2/testdata.py
+++
@@ -1,4 +0,0 @@
-from sklearn.datasets import fetch_20newgroups
-
-categories = ['comp.graphics']
-graphics_train = fetch_20newsgroups(sucset = 'train', categories = categories, shuffle=True, random_state=42)
